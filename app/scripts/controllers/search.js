8ca6bc835a299901b3f0146dfe0956e571a3dc51
Giuseppe Piccolo <giuseppe.piccolo1@gmail.com>
2015-01-11 17:25:33 +0100

    add spinner when user start to digit in search bar

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -6,6 +6,23 @@
  * Controller of the wheretoliveApp
  */
 angular.module('wheretoliveApp')
-  .controller('SearchCtrl',['$rootScope','$scope',function($rootScope,$scope){
+  .controller('SearchCtrl',['$rootScope','$scope','$log',function($rootScope,$scope,$log){
+    $scope.init = function (){
+      $scope.searchQuery = '';
+      $scope.minQueryLength = 3;
+      $scope.queryResults = []; //After search
+      $scope.isSearchStart = false; //Do not show search spinner indicator
+    };
 
+    /**
+     * Begin search
+     */
+    $scope.startSearch = function (){
+      if($scope.searchQuery.length >= $scope.minQueryLength){
+        $scope.isSearchStart = true;
+      }
+      else{
+        $scope.isSearchStart = false;
+      }
+    }
   }]);
