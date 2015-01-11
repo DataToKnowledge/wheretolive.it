226de6f50dbed9b16342fd63cf29187f2caac313
Giuseppe Piccolo <giuseppe.piccolo1@gmail.com>
2015-01-11 16:47:27 +0100

    add search controller

---
+++ app/scripts/controllers/search.js
@@ -0,0 +1,11 @@
+/**
+ * @ngdoc function
+ * @name wheretoliveApp.controller:SearchCtrl
+ * @description
+ * # SearchCtrl
+ * Controller of the wheretoliveApp
+ */
+angular.module('wheretoliveApp')
+  .controller('SearchCtrl',['$rootScope','$scope',function($rootScope,$scope){
+
+  }]);
