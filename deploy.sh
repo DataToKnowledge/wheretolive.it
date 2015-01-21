5d804b0b7cf3b806bff7e5a7aa98d9e49c72e145
fabiofumarola <fabiofumarola@gmail.com>
2015-01-21 10:01:01 +0100

    update deploy.sh to remove dist at the end

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,8 @@ git add -f dist && commit -m "update dist folder"
 echo "push to origin deploy branch"
 git subtree push --prefix dist origin deploy
 
+echo "ok remove the dist directory"
+
+git rm -r dist
 
 echo "go to the server and update"
