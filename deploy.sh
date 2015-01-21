684ad46704ff4127997cb788bb286d741549d51e
fabiofumarola <fabiofumarola@gmail.com>
2015-01-21 09:51:12 +0100

    add readme

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "build the project"
+grunt build
+
+echo "copy the dockers folder to the dist folder"
+cp dockers dist/
+
+echo "add the dist folder to git"
+git add -f dist && commit -m "update dist folder"
+
+echo "push to origin deploy branch"
+git subtree push --prefix dist origin deploy
+
+
+echo "go to the server and update"
