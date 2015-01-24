9c59a5d710cac92fe458faaeb14cc4efd2bd91fe
fabiofumarola <fabiofumarola@gmail.com>
2015-01-24 19:37:57 +0100

    update package.json

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 5000);
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running")
-})
