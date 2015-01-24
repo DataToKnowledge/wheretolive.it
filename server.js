5523a0efd92feca5fe7c2c8d28539ba10ec0dfb4
fabiofumarola <fabiofumarola@gmail.com>
2015-01-24 19:31:53 +0100

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/app'));
 app.listen(process.env.PORT || 5000);
 
 app.listen(app.get('port'), function() {
