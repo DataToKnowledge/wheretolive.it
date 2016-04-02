f8a88db088f0b50d3905173fd03ba588819edb86
fabiana <fabiana.lan@hotmail.it>
2016-04-02 21:05:01 +0200

    added elasticParser

---
+++ app/scripts/services/elasticsearch/elasticParser.js
@@ -0,0 +1,47 @@
+/**
+ * Created by fabiana on 4/2/16.
+ */
+var app = angular.module('wheretoliveApp');
+app.factory('EsParser', function () {
+
+  var parser = {};
+
+  var getAnnotations = function(jsonAnnotations){
+    var annotations = jsonAnnotations.map(function(obj){
+      return obj.surfaceForm;
+    });
+    return annotations;
+  };
+
+  var getDate = function(stringDate) {
+    return stringDate.split("T")[0]
+  };
+
+  var getDomainURL = function(url) {
+    var arr = url.split("/");
+    var res = arr[0] + "//" + arr[2]
+    return res;
+  };
+
+  parser.parseLastNews = function (json) {
+    var array = json.hits.hits;
+    var articles = array.map(function (obj) {
+      var article = {};
+      article.id = obj._id;
+      article.publisher = obj._source.publisher;
+      article.uri = obj._source.uri;
+      article.uriWebsite = obj._source.uri !== undefined ? getDomainURL(obj._source.uri) : undefined;
+      article.imageUrl = obj._source.imageUrl;
+      article.title = obj._source.title;
+      article.description = obj._source.description;
+      article.date = obj._source.date !== undefined ? getDate(obj._source.date) : undefined;
+      article.pin = obj._source.focusLocation!== undefined ?  obj._source.focusLocation.pin : undefined;
+      article.cityName = obj._source.focusLocation!== undefined ?  obj._source.focusLocation.cityName : undefined;
+      article.keywords = obj._source.keywords;
+      article.annotations = obj._source.annotations !== undefined ? getAnnotations(obj._source.annotations) : undefined;
+      return article;
+    });
+    return articles;
+  };
+    return parser;
+});
